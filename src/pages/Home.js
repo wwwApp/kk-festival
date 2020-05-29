a07283feaba556a063a9641f457f389fbe594f2f
Woo Song <ws392@drexel.edu>
2020-05-29 03:00:44 -0400

    last positioning stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -313,6 +313,7 @@ const HomeContainer = styled.div`
     position: absolute;
     bottom: 1rem;
     left: 50%;
+    transform: translateX(-50%);
     border-radius: 2.5rem;
     z-index: 3;
     display: flex;
